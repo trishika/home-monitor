81f8ccc6222ce74bc3914837eb4f0a10754122ab
Aurélien Chabot <aurelien@chabot.fr>
2013-11-01 13:11:44 +0100

    New test

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 ###### ADD
 
 curl -i -X POST --data "switch_host='todd:5000'&switch_id=2&sensor_host='todd:5000'&sensor_id=0&start_time=20&end_time=07&temp=22" http://localhost:5001/rules/
+curl -i -X POST --data "switch_host='todd:5000'&switch_id=2&sensor_host='todd:5000'&sensor_id=0&start_time=07&end_time=20&temp=22" http://localhost:5001/rules/
 
 curl -i -X GET http://localhost:5001/rules/
 
