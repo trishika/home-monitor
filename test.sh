7f1079e7e28c329e50f29db7ef1093548481a0c9
Aurélien Chabot <aurelien@chabot.fr>
2014-01-22 22:51:34 +0000

    Fix test, remove extra quote

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 ###### ADD
 
-curl -i -X POST --data "switch_host='todd:5000'&switch_id=2&sensor_host='todd:5000'&sensor_id=0&start_time=20&end_time=07&temp=22" http://localhost:5001/rules/
-curl -i -X POST --data "switch_host='todd:5000'&switch_id=2&sensor_host='todd:5000'&sensor_id=0&start_time=07&end_time=20&temp=22" http://localhost:5001/rules/
+curl -i -X POST --data "switch_host=todd:5000&switch_id=2&sensor_host=todd:5000&sensor_id=0&start_time=20&end_time=8&temp=19" http://localhost:5001/rules/
+curl -i -X POST --data "switch_host=todd:5000&switch_id=2&sensor_host=todd:5000&sensor_id=0&start_time=08&end_time=20&temp=13" http://localhost:5001/rules/
 
 curl -i -X GET http://localhost:5001/rules/
 
